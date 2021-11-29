ec42dae8667998078e9119ad4aef1f06693f176e
adushinova_ekaterina <adushinova2002@mail.ru>
2021-11-29 20:44:03 +0800

    Создала отдельный файл под классы с напитками

---
+++ Drink.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторная_работа__4
+{
+    // используемый фрукт в соке
+    public enum JuiceType { apple, orange, cherry, apricot, grapes };
+
+    // Сок
+    public class Juice
+    {
+        public JuiceType type = JuiceType.apple; // используемый фрукт
+        public bool WithPulp = false; // наличие мякоти
+    }
+
+    // вид газировки
+    public enum SodaType { lemonade, mineral };
+
+    // Газировка
+    public class Soda
+    {
+        public SodaType type = SodaType.lemonade; // вид
+        public int BubblesNumber = 0; // количество пузырьков
+
+    }
+
+    // крепость алкоголя
+    public enum AlcoholFortressType { low, medium, strong };
+
+    // тип алкоголя
+    public enum AlcoholType { whiskey, rum, wine, liqueur };
+    // Алкоголь
+    public class Alcohol
+    {
+        public AlcoholFortressType fortressType = AlcoholFortressType.low; // крепость
+        public AlcoholType type = AlcoholType.whiskey; // тип 
+    }
+}
